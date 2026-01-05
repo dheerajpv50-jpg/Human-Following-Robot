87c46899c2544ee6637f2a6e0eb1e556002f9d18
dheerajpv50-jpg <dheerajpv50@gmail.com>
2026-01-05 09:40:23 +0530

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,2 +1,71 @@
 // Human Following Robot
 // Arduino code will be added here
+// Human Following Robot using Ultrasonic Sensor
+// Arduino UNO + L298N
+
+#define trigPin 9
+#define echoPin 10
+
+#define ENA 5
+#define IN1 6
+#define IN2 7
+#define IN3 8
+#define IN4 11
+#define ENB 3
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  Serial.println(distance);
+
+  if (distance > 10 && distance < 30) {
+    moveForward();
+  } else {
+    stopRobot();
+  }
+
+  delay(100);
+}
+
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 150);
+  analogWrite(ENB, 150);
+}
+
+void stopRobot() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+
